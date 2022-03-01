500439ef07366f67053c41b012c05ed846e9503c
程崇臻 <15939054361@163.com>
2022-03-01 22:58:37 +0800

    新增 login 页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 
-const router = [
+const router: RouteRecordRaw[] = [
   {
     path: '/',
     redirect: '/home'
+  },
+  {
+    path: '/login',
+    component: () => import('@/view/login/loginIndex.vue')
   },
   {
     path: '/home',
