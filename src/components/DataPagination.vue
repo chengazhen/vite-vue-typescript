d956f8cfa26fde32715d3933788361a2aaa31bf0
chengchongzhen <chengchongzhen@hexfuture.net>
2022-03-03 14:36:06 +0800

    封装分页组件

---
+++ src/components/DataPagination.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="pagination-wrapper">
+        <el-pagination
+            v-model:page-size="pageSize"
+            :small="true"
+            v-model:current-page="current"
+            :background="true"
+            layout="sizes, total, prev, pager, next"
+            :total="props.total"
+            :page-sizes="[20, 40, 60, 80]"
+        ></el-pagination>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed, nextTick } from 'vue';
+
+interface propsType {
+    pageSize?: number,
+    total: number,
+    currentPage?: number,
+}
+const props = withDefaults(defineProps<propsType>(), {
+    pageSize: 20,
+    total: 40,
+    currentPage: 1
+})
+
+const emits = defineEmits(['update:pageSize', 'update:currentPage', 'change'])
+
+const change = () => {
+    nextTick(() => {
+        emits('change', { page: current.value, size: pageSize.value })
+    })
+}
+
+const current = computed<number>({
+    get() {
+        return props.currentPage
+    }, set(value) {
+        change()
+        emits('update:currentPage', value)
+    }
+})
+
+const pageSize = computed({
+    get() {
+        return props.pageSize
+    }, set(value) {
+        change()
+        emits('update:pageSize', value)
+    }
+})
+
+
+// const propsRefs = toRefs(props)
+
+
+</script>
+
+<style scoped>
+.pagination-wrapper {
+    padding: 20px;
+    border: 1px solid rgb(216, 206, 206);
+}
+</style>
